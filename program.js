3d33e1c6728351815d694b5ad0b1ffa31d195476
Srikanth-SM <srisiro26.pec@gmail.com>
2018-04-30 12:15:18 +0530

    Fullfill a promise

--- program.js
+++ program.js
@@ -1,4 +1,11 @@
+'use strict'
+require('es6-promise');
+//      setTimeout(()=>{
+//	console.log("TIMED OUT!");
+//},300);
 
-setTimeout(()=>{
-	console.log("TIMED OUT!");
-},300)
+let promise = new Promise(function(fulfill,reject){
+	setTimeout(function(){
+	fulfill('FULFILLED!');
+},300);
+}).then(result=>console.log(result));
