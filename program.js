3cec5daebd71f6e24a92310a464fc74a983a26e8
Srikanth-SM <srisiro26.pec@gmail.com>
2018-04-30 13:30:07 +0530

    To reject or not to reject

--- program.js
+++ program.js
@@ -4,8 +4,27 @@ require('es6-promise');
 //	console.log("TIMED OUT!");
 //},300);
 
-let     promise = new Promise(function(fulfill,reject){
+/*let     promise = new Promise(function(fulfill,reject){
 	setTimeout(function(){
-	fulfill('FULFILLED!');
+	reject(new Error('REJECTED!'));
 },300);
-}).then(result=>console.log(result));
+})
+.then(result=>console.log(result),reject);
+
+
+function reject(error){
+	console.log(error.message);
+}*/
+
+//To reject or not to reject
+let promise = new Promise(function(resolve,reject){
+	resolve('I FIRED');
+	reject(new Error('I AM NOT FIRED'));
+});
+
+function OnReject(error){
+        console.log(err.message);
+}
+
+promise.then(console.log,OnReject);
+
